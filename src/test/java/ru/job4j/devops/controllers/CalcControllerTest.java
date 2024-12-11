3c2ddad55b02a0ac74a3dea9baa2f45ce714a78f
parsentev <parsentev@yandex.ru>
2024-12-11 17:09:50 +0300

    Add JUnit test with negative numbers

--- src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
+++ src/test/java/ru/job4j/devops/controllers/CalcControllerTest.java
@@ -17,4 +17,13 @@ class CalcControllerTest {
         assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
         assertThat(output.getBody()).isEqualTo(expected);
     }
+
+    @Test
+    public void whenNegativeNumber() {
+        var input = new TwoArgs(-1, -1);
+        var expected = new Result(-2);
+        var output = new CalcController().summarise(input);
+        assertThat(output.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(output.getBody()).isEqualTo(expected);
+    }
 }
