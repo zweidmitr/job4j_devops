4d45aefa646a21b2d2877cb265147a6e932b193f
peterarsentev <parsentev@yandex.ru>
2025-01-30 12:40:19 +0300

    Kafka

--- src/integrationTest/java/ru/job4j/devops/config/ContainersConfig.java
+++ src/integrationTest/java/ru/job4j/devops/config/ContainersConfig.java
@@ -1,8 +1,6 @@
 package ru.job4j.devops.config;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -13,25 +11,25 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public abstract class ContainersConfig {
-    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+    private static final PostgreSQLContainer<?> POSTGRE_SQL_CONTAINER = new PostgreSQLContainer<>(
             "postgres:16-alpine"
     );
 
-    private static final KafkaContainer kafka = new KafkaContainer(
+    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer(
             DockerImageName.parse("apache/kafka:3.7.2")
     );
 
     static {
-        postgres.start();
-        kafka.start();
+        POSTGRE_SQL_CONTAINER.start();
+        KAFKA_CONTAINER.start();
     }
 
     @DynamicPropertySource
     public static void configureProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.kafka.bootstrap-servers", KAFKA_CONTAINER::getBootstrapServers);
+        registry.add("spring.datasource.url", POSTGRE_SQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRE_SQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRE_SQL_CONTAINER::getPassword);
     }
 }
